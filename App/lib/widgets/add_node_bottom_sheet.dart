03e1be6a621988e964f43d58f2308a64c16047ac
Mahdi salah <saaess33@gmail.com>
2026-01-29 01:49:02 +0300

    finish notes bottom  sheet

--- App/lib/widgets/add_node_bottom_sheet.dart
+++ App/lib/widgets/add_node_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qq/widgets/custom_botton.dart';
 import 'package:qq/widgets/custom_text_filed.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -7,15 +8,21 @@ class AddNoteBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16),
-      child: Column(children: [
-    const    SizedBox(height: 32), 
-        CustomTextFiled(hint: "title",),
-   const    SizedBox(height: 16),
-        CustomTextFiled(hint: "content",maxLines: 5,)
-        ],
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 32),
+            CustomTextFiled(hint: "title"),
+            const SizedBox(height: 16),
+            CustomTextFiled(hint: "content", maxLines: 5),
+       SizedBox(height: 60,),
+      
+            CustomBotton(),
+          SizedBox(height: 16,)
+          ],
         ),
+      ),
     );
   }
 }
---
+++ App/lib/widgets/custom_botton.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:qq/constants.dart';
+
+class CustomBotton extends StatelessWidget {
+  const CustomBotton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: kPrimaycolor,
+        borderRadius: BorderRadius.circular(16)
+      ),
+width: MediaQuery.of(context).size.width,
+height: 55,
+child: Center(
+child: Text("Add",style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),),
+
+),
+
+    );
+  }
+}
