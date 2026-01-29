997d0381c725cc6ad2e11716bb4a472d7c0288c8
Mahdi salah <saaess33@gmail.com>
2026-01-29 23:01:32 +0300

    add add npte method to add note cubit

--- App/lib/cubits/add_note_cubit.dart/cubit/add_note_cubit.dart
+++ App/lib/cubits/add_note_cubit.dart/cubit/add_note_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:qq/constants.dart';
 import 'package:qq/models/note_model.dart';
 
 part 'add_note_cubit_state.dart';
@@ -7,9 +9,15 @@ part 'add_note_cubit_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+  addNote(NoteModel note) async {
+    emit(AddNoteLoding());
+    try {
+      var notesBox = Hive.box<NoteModel>(knotesBox);
 
-  addNote(NoteModel note){
-
-    
+      emit(AddNoteSuccess());
+      await notesBox.add(note);
+    } on Exception catch (e) {
+      AddNoteFailure(e.toString());
+    }
   }
 }
